494a87fe1837477aa8df3309f28a7eac6b2f71d4
Peter Qin <pqin@users.noreply.github.com>
2017-09-20 17:45:09 -0600

    Delete Record Action shows Dialog message with newlines

--- src/action/DeleteRecordAction.java
+++ src/action/DeleteRecordAction.java
@@ -14,7 +14,7 @@ public class DeleteRecordAction extends RecordAction {
 	/**
 	 * Regex that matches ISBD punctuation in titles, keeping delimiters at end of tokens.
 	 */
-	private static final Pattern TITLE_ISBD_REGEX = Pattern.compile("(?<=[:/])");
+	private static final Pattern TITLE_ISBD_REGEX = Pattern.compile("(?<=[:/;=])");
 	
 	public DeleteRecordAction(Catalogue data, Component owner){
 		super("Delete Record", data, owner);
@@ -39,9 +39,15 @@ public class DeleteRecordAction extends RecordAction {
 			// generate message: split into multiple lines along ISBD punctuation if needed
 			title = record.getTitle();
 			if (title.length() > MAX_TITLE_LENGTH){
-				title = String.join("\n", TITLE_ISBD_REGEX.split(title));
+				String[] lines = TITLE_ISBD_REGEX.split(title);
+				for (int i = 0; i < lines.length; ++i){
+					if (lines[i].length() > MAX_TITLE_LENGTH){
+						lines[i] = lines[i].substring(0, MAX_TITLE_LENGTH) + "...";
+					}
+				}
+				title = String.join("</p><p>", lines);
 			}
-			message = String.format("Delete Record:%n%s", title);
+			message = String.format("<html><p>Delete Record:</p><p>%s</p></html>", title);
 			formDialog.setContent(message);
 			int option = formDialog.showDialog();
 			if (option == 0){
