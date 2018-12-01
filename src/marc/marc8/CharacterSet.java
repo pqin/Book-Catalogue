5782bd758dcb3028a885a5121bd4781a5b87850b
Peter Qin <pqin@users.noreply.github.com>
2018-12-01 14:08:48 -0700

    Replaced table (array) with Map.

--- src/marc/marc8/CharacterSet.java
+++ src/marc/marc8/CharacterSet.java
@@ -1,6 +1,8 @@
 package marc.marc8;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CharacterSet {
 	public static final char UNKNOWN_CHAR = '\uFFFD';
@@ -13,7 +15,9 @@ public class CharacterSet {
 	protected int defaultGraphicSet;
 	protected int[] buffer;
 	protected int[] base;
-	private char[] table;
+	// private char[] table;
+	private Map<Integer, Character> byteMap;
+	private Map<Character, Integer> charMap;
 
 	public CharacterSet(String name, final int bytesPerChar, final int offset, final int length) {
 		this.name = name;
@@ -33,8 +37,11 @@ public class CharacterSet {
 			base[i] = len;
 			len *= length;
 		}
-		table = new char[length*base[this.bytesPerChar - 1]];
 		defaultGraphicSet = 0;
+		
+		// table = new char[length*base[this.bytesPerChar - 1]];
+		byteMap = new HashMap<Integer, Character>();
+		charMap = new HashMap<Character, Integer>();
 	}
 
 	public final String getName() {
@@ -42,8 +49,7 @@ public class CharacterSet {
 	}
 
 	public final int getBytesPerChar() {
-		int i = bytesPerChar;
-		return i;
+		return (int)bytesPerChar;
 	}
 
 	public final void reset() {
@@ -54,13 +60,12 @@ public class CharacterSet {
 	public int getOffset(){
 		return offset;
 	}
-	public int setTable(char[] value) {
-		final int difference = value.length - table.length;
-		table = Arrays.copyOf(value, table.length);
-		for (int i = value.length; i < table.length; ++i){
-			table[i] = UNKNOWN_CHAR;
+	public void setTable(char[] value) {
+		byteMap.clear();
+		charMap.clear();
+		for (int i = 0; i < value.length; ++i){
+			byteMap.put(i, value[i]);
 		}
-		return difference;
 	}
 	public final int getTableLength(){
 		return length;
@@ -78,30 +83,23 @@ public class CharacterSet {
 
 	public final char decode(int b) {
 		char c = UNDERFLOW;
+		if (b < 0 || b < offset){
+			throw new IllegalArgumentException("Decoded byte is negative.");
+		}
 		buffer[counter] = (b - offset)*base[counter];
 		if (counter == bytesPerChar - 1){
 			int index = 0;
 			for (int i = 0; i < bytesPerChar; ++i){
 				index += buffer[i];
 			}
-			if (index < 0){
-				System.out.printf("%H > %H%n", b, index);
-			}
-			c = table[index];
+			c = byteMap.getOrDefault(index, UNKNOWN_CHAR);
 		}
 		counter = (counter + 1) % bytesPerChar;
 		return c;
 	}
 
 	public boolean contains(final char c) {
-		boolean match = false;
-		for (int i = 0; i < table.length; ++i){
-			if (table[i] == c){
-				match = true;
-				break;
-			}
-		}
-		return match;
+		return byteMap.containsValue(c);
 	}
 
 	public byte[] encode(final char c, final int g) {
@@ -114,19 +112,15 @@ public class CharacterSet {
 			b = new byte[bytesPerChar];
 		}
 		int q, r;
-		for (int i = 0; i < table.length; ++i){
-			if (table[i] == c){
-				for (int k = base.length - 1; k >= 0; --k){
-					q = i / base[k];
-					r = i - (q * base[k]);
-					b[k] = (byte) (q + offset);
-					if (g > 0){
-						b[k] = (byte) (b[k] | 0x80);
-					}
-					i = r;
-				}
-				break;
+		int index = charMap.get(c);
+		for (int k = base.length - 1; k >= 0; --k){
+			q = index / base[k];
+			r = index - (q * base[k]);
+			b[k] = (byte) (q + offset);
+			if (g > 0){
+				b[k] = (byte) (b[k] | 0x80);
 			}
+			index = r;
 		}
 		return b;
 	}
